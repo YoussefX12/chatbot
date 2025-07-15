271a4d2ec90693f5a21bdaa60f6902f862a9f496
YoussefX12 <161139156+YoussefX12@users.noreply.github.com>
2025-07-15 15:22:08 +0100

    Update chatbot_api.py

--- chatbot_api.py
+++ chatbot_api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from chatbot import ask_openrouter, fetch_all_data
+API_KEY = "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"
 
 app = FastAPI()
 context_data = fetch_all_data()
